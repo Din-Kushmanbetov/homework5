1b633f780adc28eb7d517a0bef5bc5ee3a3b5840
Din-Kushmanbetov <aiddenpes@gmail.com>
2024-05-17 08:48:57 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['black', 'red','white','blue','yellow']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'purple'
+print(my_list)
+
+my_dict = {'cup':'glass', 'blouse': 'woolen', 'chair':'wooden'}
+print(my_dict)
+print(my_dict['blouse'])
+my_dict['cup'] = 'plastic'
+print(my_dict)
